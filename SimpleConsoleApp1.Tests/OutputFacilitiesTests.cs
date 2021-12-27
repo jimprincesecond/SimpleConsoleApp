0187520d767c8062ceb6e62d489e756aa2435e17
jimprince99second <jimprince99second@gmail.com>
2021-12-27 14:33:19 -0800

    initial version

---
+++ SimpleConsoleApp1.Tests/OutputFacilitiesTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SimpleConsoleApp1.Tests
+{
+    public class OutputFacilitiesTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var o = new OutputFacilities();
+
+            var output = o.print("Hello world");
+            
+            Assert.True(output);
+        }
+    }
+}
---
+++ SimpleConsoleApp1/OutputFacilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleConsoleApp1
+{
+    public class OutputFacilities
+    {
+        public bool print(string input)
+        {
+            Console.WriteLine(input);
+            return true;
+        }
+    }
+}
