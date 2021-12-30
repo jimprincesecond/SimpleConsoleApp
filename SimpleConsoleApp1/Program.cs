e1fb060c4b3775d8440a34558ac9f2a4595dd391
jimprince99second <jimprince99second@gmail.com>
2021-12-30 17:21:27 +0000

    more changes

--- SimpleConsoleApp1/Program.cs
+++ SimpleConsoleApp1/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SimpleConsoleApp1
 {
     class Program
     {
+
+        [ExcludeFromCodeCoverage]
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
